8b68c5a02733b42652ed40ab46ba1e7bc09501a2
Grzegorz <g.w.mika@gmail.com>
2020-06-07 18:29:31 +0200

    cleaning

--- Simulations/bessel_zeros/bessel_zeros.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import os
-
-zeros = np.loadtxt('./bessel_zeros_short.txt')
-np.save('bessel_zeros_short', zeros)
-
-if os.path.exists('./bessel_zeros_short.txt'):
-    os.remove('./bessel_zeros_short.txt')
