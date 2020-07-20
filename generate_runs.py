4caf931b202b43d798ee7795a9fdc027bd38911b
Grzegorz <g.w.mika@gmail.com>
2020-07-20 15:52:10 +0200

    better filtering in generate_runs

--- generate_runs.py
+++ generate_runs.py
@@ -3,7 +3,7 @@ import os
 methods = ['landweber', 'tikhonov', 'tsvd']
 files_to_run = os.listdir('.')
 
-files_to_run = [f for f in files_to_run if any(m in f for m in methods)]
+files_to_run = [f for f in files_to_run if any(m in f for m in methods) and '.py' in f]
 files_to_run.sort()
 
 with open('run_simulations.sh', 'w+') as f:
